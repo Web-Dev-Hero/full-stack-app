03327d590c4899cade1ca7c3c62ee1ca3cebac32
devNikhil <168222942+Web-Dev-Hero@users.noreply.github.com>
2025-02-12 17:50:32 +0530

    add express js

--- src/server.ts
+++ src/server.ts
@@ -1,2 +1,14 @@
 import { Config } from './config';
-console.log(Config.PORT);
+import app from './app';
+
+const server = () => {
+    const PORT = Config.PORT;
+    try {
+        app.listen(PORT, () =>
+            console.log(`Server is running on port ${PORT}`),
+        );
+    } catch (err) {
+        console.error('Error starting server', err);
+    }
+};
+server();
