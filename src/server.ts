faf5327cecf170e21faa182bf89cb8953bac4c49
devNikhil <168222942+Web-Dev-Hero@users.noreply.github.com>
2025-02-14 19:07:34 +0530

    add logger

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,13 @@
 import { Config } from './config';
 import app from './app';
+import logger from './config/logger';
 
 const server = () => {
     const PORT = Config.PORT;
     try {
-        app.listen(PORT, () =>
-            console.log(`Server is running on port ${PORT}`),
-        );
+        app.listen(PORT, () => {
+            logger.info('serveer listing in port', { port: PORT });
+        });
     } catch (err) {
         console.error('Error starting server', err);
     }
